6bd4041bcaaadc527eefd722d3aaa0f2a077613f
Markku Koskela <markku.koskela@fmi.fi>
2017-09-19 17:54:19 +0300

    A new client

---
+++ src/clients/cs_getContentParamKeyListByGenerationId.cpp
@@ -0,0 +1,88 @@
+#include "contentServer/corba/client/ClientImplementation.h"
+#include "contentServer/http/client/ClientImplementation.h"
+#include "contentServer/redis/RedisImplementation.h"
+#include "grid-files/common/Exception.h"
+#include "grid-files/common/GeneralFunctions.h"
+#include <vector>
+
+
+using namespace SmartMet;
+
+
+int main(int argc, char *argv[])
+{
+  try
+  {
+    if (argc < 4)
+    {
+      fprintf(stdout,"USAGE: cs_getContentParamListByGenerationId <sessionId> <generationId> <paramKeyType> [[-http <url>]|[-redis <address> <port> <tablePrefix>]]\n");
+      return -1;
+    }
+
+    T::SessionId sessionId = (SmartMet::T::SessionId)atoll(argv[1]);
+    uint generationId = (uint)atoll(argv[2]);
+    T::ParamKeyType paramKeyType = (T::ParamKeyType)atoll(argv[3]);
+    std::set<std::string> paramKeyList;
+
+    int result = 0;
+    unsigned long long startTime = 0;
+    unsigned long long endTime = 0;
+
+    if (argc == 6  &&  strcmp(argv[argc-2],"-http") == 0)
+    {
+      ContentServer::HTTP::ClientImplementation service;
+      service.init(argv[argc-1]);
+
+      startTime = getTime();
+      result = service.getContentParamKeyListByGenerationId(sessionId,generationId,paramKeyType,paramKeyList);
+      endTime = getTime();
+    }
+    else
+    if (argc > 4  &&  strcmp(argv[argc-4],"-redis") == 0)
+    {
+      ContentServer::RedisImplementation service;
+      service.init(argv[argc-3],atoi(argv[argc-2]),argv[argc-1]);
+
+      startTime = getTime();
+      result = service.getContentParamKeyListByGenerationId(sessionId,generationId,paramKeyType,paramKeyList);
+      endTime = getTime();
+    }
+    else
+    {
+      char *serviceIor = getenv("SMARTMET_CS_IOR");
+      if (serviceIor == NULL)
+      {
+        fprintf(stdout,"SMARTMET_CS_IOR not defined!\n");
+        return -2;
+      }
+
+      ContentServer::Corba::ClientImplementation service;
+      service.init(serviceIor);
+
+      startTime = getTime();
+      result = service.getContentParamKeyListByGenerationId(sessionId,generationId,paramKeyType,paramKeyList);
+      endTime = getTime();
+    }
+
+    if (result != 0)
+    {
+      fprintf(stdout,"ERROR (%d) : %s\n",result,ContentServer::getResultString(result).c_str());
+      return -3;
+    }
+
+    // ### Result:
+    for (auto it=paramKeyList.begin(); it!=paramKeyList.end(); ++it)
+      printf("%s\n",it->c_str());
+
+    printf("\nTIME : %f sec\n\n",(float)(endTime-startTime)/1000000);
+
+    return 0;
+  }
+  catch (SmartMet::Spine::Exception& e)
+  {
+    SmartMet::Spine::Exception exception(BCP,"Service call failed!",NULL);
+    exception.printError();
+    return -4;
+  }
+}
+
