2823b15b3c74e8e1f056ffec4fbb469d54f8fbb6
Markku Koskela <markku.koskela@fmi.fi>
2018-09-27 11:49:45 +0300

    New client programs for the Data Server

---
+++ src/clients/ds_getGridValueVector.cpp
@@ -0,0 +1,73 @@
+#include "grid-content/contentServer/corba/client/ClientImplementation.h"
+#include "grid-content/dataServer/corba/client/ClientImplementation.h"
+#include "grid-files/common/Exception.h"
+#include "grid-files/common/GeneralFunctions.h"
+
+using namespace SmartMet;
+
+
+int main(int argc, char *argv[])
+{
+  try
+  {
+    char *serviceIor = getenv("SMARTMET_DS_IOR");
+    if (serviceIor == nullptr)
+    {
+      fprintf(stdout,"SMARTMET_DS_IOR not defined!\n");
+      return -2;
+    }
+
+    if (argc != 5)
+    {
+      fprintf(stdout,"USAGE: ds_getGridValueVector <sessionId> <fileId> <messageIndex> <flags>\n");
+      return -1;
+    }
+
+
+    // ### Session:
+    T::SessionId sessionId = toInt64(argv[1]);
+
+
+    // ### Creating a dataServer client:
+
+    DataServer::Corba::ClientImplementation dataServer;
+    dataServer.init(serviceIor);
+
+    // ### Calling the dataServer:
+
+    uint fileId = toInt64(argv[2]);
+    uint messageIndex = toInt64(argv[3]);
+    uint flags = toInt64(argv[4]);
+    T::ParamValue_vec values;
+
+    unsigned long long startTime = getTime();
+    int result = dataServer.getGridValueVector(sessionId,fileId,messageIndex,flags,values);
+    unsigned long long endTime = getTime();
+
+
+    if (result != 0)
+    {
+      fprintf(stdout,"ERROR (%d) : %s\n",result,DataServer::getResultString(result).c_str());
+      return -5;
+    }
+
+    // ### Printing the result:
+
+    uint cnt = values.size();
+    for (uint t=0; t<cnt; t++)
+    {
+      printf("%06d;%f\n",t,values[t]);
+    }
+
+    printf("\nTIME : %f sec\n\n",(float)(endTime-startTime)/1000000);
+
+    return 0;
+  }
+  catch (SmartMet::Spine::Exception& e)
+  {
+    SmartMet::Spine::Exception exception(BCP,"Service call failed!",nullptr);
+    exception.printError();
+    return -7;
+  }
+}
+
---
+++ src/clients/ds_getGridValueVectorByTime.cpp
@@ -0,0 +1,76 @@
+#include "grid-content/contentServer/corba/client/ClientImplementation.h"
+#include "grid-content/dataServer/corba/client/ClientImplementation.h"
+#include "grid-files/common/Exception.h"
+#include "grid-files/common/GeneralFunctions.h"
+
+using namespace SmartMet;
+
+
+int main(int argc, char *argv[])
+{
+  try
+  {
+    char *serviceIor = getenv("SMARTMET_DS_IOR");
+    if (serviceIor == nullptr)
+    {
+      fprintf(stdout,"SMARTMET_DS_IOR not defined!\n");
+      return -2;
+    }
+
+    if (argc != 8)
+    {
+      fprintf(stdout,"USAGE: ds_getGridValueVector <sessionId> <fileId1> <messageIndex1> <fileId2> <messageIndex2> <newTime> <flags>\n");
+      return -1;
+    }
+
+
+    // ### Session:
+    T::SessionId sessionId = toInt64(argv[1]);
+
+
+    // ### Creating a dataServer client:
+
+    DataServer::Corba::ClientImplementation dataServer;
+    dataServer.init(serviceIor);
+
+    // ### Calling the dataServer:
+
+    uint fileId1 = toInt64(argv[2]);
+    uint messageIndex1 = toInt64(argv[3]);
+    uint fileId2 = toInt64(argv[4]);
+    uint messageIndex2 = toInt64(argv[5]);
+    std::string newTime = argv[6];
+    uint flags = toInt64(argv[7]);
+    T::ParamValue_vec values;
+
+    unsigned long long startTime = getTime();
+    int result = dataServer.getGridValueVectorByTime(sessionId,fileId1,messageIndex1,fileId2,messageIndex2,newTime,flags,values);
+    unsigned long long endTime = getTime();
+
+
+    if (result != 0)
+    {
+      fprintf(stdout,"ERROR (%d) : %s\n",result,DataServer::getResultString(result).c_str());
+      return -5;
+    }
+
+    // ### Printing the result:
+
+    uint cnt = values.size();
+    for (uint t=0; t<cnt; t++)
+    {
+      printf("%06d;%f\n",t,values[t]);
+    }
+
+    printf("\nTIME : %f sec\n\n",(float)(endTime-startTime)/1000000);
+
+    return 0;
+  }
+  catch (SmartMet::Spine::Exception& e)
+  {
+    SmartMet::Spine::Exception exception(BCP,"Service call failed!",nullptr);
+    exception.printError();
+    return -7;
+  }
+}
+
