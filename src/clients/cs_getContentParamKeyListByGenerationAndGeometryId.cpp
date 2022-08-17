8f72b10e2047879db6517762eee454af47be0d2d
Markku Koskela <markku.koskela@fmi.fi>
2022-08-17 13:15:27 +0300

    Added new client programs for testing purposes

---
+++ src/clients/cs_getContentParamKeyListByGenerationAndGeometryId.cpp
@@ -0,0 +1,96 @@
+#include "grid-content/contentServer/corba/client/ClientImplementation.h"
+#include "grid-content/contentServer/http/client/ClientImplementation.h"
+// #include "grid-content/contentServer/postgres/PostgresImplementation.h"
+#include "grid-content/contentServer/redis/RedisImplementation.h"
+#include <macgyver/Exception.h>
+#include "grid-files/common/GeneralFunctions.h"
+#include <vector>
+
+
+using namespace SmartMet;
+
+
+int main(int argc, char *argv[])
+{
+  try
+  {
+    if (argc < 4)
+    {
+      fprintf(stdout,"USAGE: cs_getContentParamListByGenerationAndGeometryId <sessionId> <generationId> <geometryId> <paramKeyType> [[-http <url>]|[-redis <address> <port> <tablePrefix>]]\n");
+      return -1;
+    }
+
+    T::SessionId sessionId = toInt64(argv[1]);
+    uint generationId = toInt64(argv[2]);
+    uint geometryId = toInt64(argv[3]);
+    T::ParamKeyType paramKeyType = toInt64(argv[4]);
+    std::set<std::string> paramKeyList;
+
+    ContentServer::ServiceInterface *service = nullptr;
+
+    if (strcmp(argv[argc-2],"-http") == 0)
+    {
+      ContentServer::HTTP::ClientImplementation *httpClient = new ContentServer::HTTP::ClientImplementation();
+      httpClient->init(argv[argc-1]);
+      service = httpClient;
+    }
+    else
+    if (argc > 4  &&  strcmp(argv[argc-4],"-redis") == 0)
+    {
+      ContentServer::RedisImplementation *redis = new ContentServer::RedisImplementation();
+      redis->init(argv[argc-3],toInt64(argv[argc-2]),argv[argc-1]);
+      service = redis;
+    }
+    else
+    {
+      char *serviceIor = getenv("SMARTMET_CS_IOR");
+
+      if (strcmp(argv[argc-2],"-ior") == 0)
+        serviceIor = argv[argc-1];
+
+      if (serviceIor == nullptr)
+      {
+        fprintf(stdout,"Service IOR not defined!\n");
+        return -2;
+      }
+
+      ContentServer::Corba::ClientImplementation *corbaClient = new ContentServer::Corba::ClientImplementation();
+      corbaClient->init(serviceIor);
+      service = corbaClient;
+    }
+
+
+    if (service == nullptr)
+    {
+      fprintf(stdout,"ERROR : Service not defined!\n");
+      return -3;
+    }
+
+    unsigned long long startTime = getTime();
+    int result = service->getContentParamKeyListByGenerationAndGeometryId(sessionId,generationId,geometryId,paramKeyType,paramKeyList);
+    unsigned long long endTime = getTime();
+
+    if (result != 0)
+    {
+      fprintf(stdout,"ERROR (%d) : %s\n",result,ContentServer::getResultString(result).c_str());
+      return -3;
+    }
+
+    // ### Result:
+    for (auto it=paramKeyList.begin(); it!=paramKeyList.end(); ++it)
+      printf("%s\n",it->c_str());
+
+    printf("\nTIME : %f sec\n\n",(float)(endTime-startTime)/1000000);
+
+    delete service;
+
+    return 0;
+  }
+  catch (Fmi::Exception& e)
+  {
+    Fmi::Exception exception(BCP,"Service call failed!",nullptr);
+    exception.printError();
+    return -4;
+  }
+}
+
